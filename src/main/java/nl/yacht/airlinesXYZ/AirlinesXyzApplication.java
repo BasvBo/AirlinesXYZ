db9aa2515a3870c875f6e94bcfb924b01ec7d415
BasvBo <37073686+BasvBo@users.noreply.github.com>
2018-04-03 09:22:01 +0200

    Spring boot setup

---
+++ src/main/java/nl/yacht/airlinesXYZ/AirlinesXyzApplication.java
@@ -0,0 +1,12 @@
+package nl.yacht.airlinesXYZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesXyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesXyzApplication.class, args);
+	}
+}
